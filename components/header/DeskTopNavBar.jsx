544e57d3c0672303ded472f946ac283ebbb473e9
ibrahimhassona <ibrahemhasson@gmail.com>
2024-09-26 16:31:50 +0300

    the first commit for ZKR

---
+++ components/header/DeskTopNavBar.jsx
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
+
+
+const DeskTopNavBar = () => {
+    return (
+        <div className=''>
+            {/* ---------- Logo ---------- */}
+            {/* <Image
+                src="/zkr.png"
+                alt="Logo"
+                width={150}
+                height={150}
+                className=''
+            /> */}
+            <span className=' bg-red-200'>Hello</span>
+        </div>
+    )
+}
+
+export default DeskTopNavBar;
