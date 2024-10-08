f943e0a6df0ac9b9ab7436f21c18905ee80ba895
ibrahimhassona <ibrahemhasson@gmail.com>
2024-10-08 11:12:53 +0300

    Handle The steps of endPray

--- components/endPray/MobileEndPray.jsx
+++ components/endPray/MobileEndPray.jsx
@@ -1,58 +1,66 @@
 "use client";
 import React, { useState } from "react";
 import { endPrayData } from "@/utils/endPray";
+import PopupAlert from "./PopupAlert";
 
 const MobileEndPray = () => {
-    const [currentData, setCurrentData] = useState(endPrayData[0]);
-    const [count, setCount] = useState(1);
-    const [index, setIndex] = useState(0); // Using state for index
-  
-    const handleButtonClick = () => {
-      // Ensure endPrayData exists and has elements
-      if (endPrayData && endPrayData.length > 0) {
-        const currentItem = endPrayData[index];
-  
-        if (count >= currentItem.count) {
-          // Move to the next item if the count is met
-          const nextIndex = index + 1;
-  
-          // If there's a next item, update the state
-          if (nextIndex < endPrayData.length) {
-            setIndex(nextIndex);
-            setCurrentData(endPrayData[nextIndex]);
-            setCount(1); // Reset count for the next item
-          }
-        } else {
-          // Increment count
-          setCount(count + 1);
+  const [currentData, setCurrentData] = useState(endPrayData[0]);
+  const [count, setCount] = useState(1);
+  const [index, setIndex] = useState(0); // Using state for index
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+  const handlePopupClose = () => {
+    setIsPopupVisible(false); // This function will hide the popup
+  };
+  const handleButtonClick = () => {
+    if (index == endPrayData.length-1) {
+      setIsPopupVisible(true)
+    }
+    // Ensure endPrayData exists and has elements
+    if (endPrayData && endPrayData.length > 0) {
+      const currentItem = endPrayData[index];
+
+      if (count >= currentItem.count) {
+        // Move to the next item if the count is met
+        const nextIndex = index + 1;
+
+        // If there's a next item, update the state
+        if (nextIndex < endPrayData.length) {
+          setIndex(nextIndex);
+          setCurrentData(endPrayData[nextIndex]);
+          setCount(1); // Reset count for the next item
         }
+      } else {
+        // Increment count
+        setCount(count + 1);
       }
-  
-      console.log("Count ===>", count);
-      console.log("Index ===>", index);
-    };
+    }
+
+    console.log("Count ===>", count);
+    console.log("Index ===>", index);
+  };
   return (
     <div className="h-[calc(100vh-(70px+60px))] overflow-hidden text-xs flex-col justify-between max-lg:flex hidden">
       <div className="grid grid-cols-6">
         {/* ----- Progress Section ----- */}
         <div className="flex flex-col items-start gap-2 p-2 col-span-2 border-gray-200">
           {endPrayData.map((item) => (
-            <div className="flex items-center w-full" key={item.id}>
+            <div className={`flex items-center cust-trans w-full ${currentData.id >= item.id ? 'animate-flip-up' : ''}`} key={item.id}>
               <span
-                className={`cust-trans ${
-                  currentData.id >= item.id
+                className={`cust-trans flex items-center gap-1 ${currentData.id >= item.id
                     ? "text-primary-dark font-bold"
-                    : "text-gray-400"
-                }`}
+                    : "text-custGray"
+                  }`}
               >
-                {`${item.id}- ${item.title} (${item.count})`}
+                <span className={`h-[8px] block w-[8px] border border-custGray  rounded-full  ${currentData.id >= item.id ? 'border-primary-dark bg-primary-dark' : 'bg-white'}`}></span>{`${item.title} `}
+                <span className='text-[9px]'>{`(${item.count})`}</span>
               </span>
             </div>
           ))}
         </div>
         {/* ----- Repeat Section ----- */}
-        <div className="flex flex-col col-span-4 gap-2 p-2">
-          <span className="text-primary-dark my-2 font-bold w-full ">
+        <div className="flex flex-col col-span-4 gap-2 p-2 ">
+          <span className="text-primary-dark my-2 text-sm font-bold w-full ">
             {count} مِنْ {currentData.count}
           </span>
           <div
@@ -67,7 +75,7 @@ const MobileEndPray = () => {
             {Array.from({ length: count }).map((_, index) => (
               <span
                 key={index}
-                className=" text-primary-dark animate-jump-in h-fit"
+                className=" text-primary-dark animate-flip-up h-fit"
               >
                 {currentData.text}
               </span>
@@ -83,8 +91,12 @@ const MobileEndPray = () => {
           aria-label="Start Prayer"
         >
           {currentData.title}
+          <span className='mx-1'>{`{${count}}`}</span>
         </button>
       </div>
+      {isPopupVisible && (
+        <PopupAlert isVisible={isPopupVisible} onClose={handlePopupClose} />
+      )}
     </div>
   );
 };
--- components/header/DeskTopNavBar.jsx
+++ components/header/DeskTopNavBar.jsx
@@ -10,7 +10,7 @@ const DeskTopNavBar = ({ links }) => {
     const routerpath = router.split('/'[1])
     console.log("url", router.split('/')[1])
     return (
-        <div className=' flex flex-row items-center justify-between cont bg-white shadow-sm m-auto'>
+        <div className=' flex flex-row items-center max-sm:text-xs justify-between cont bg-white  shadow-sm m-auto'>
             {/* ---------- Logo ---------- */}
             <Image
                 src="/zkr.png"
@@ -22,7 +22,7 @@ const DeskTopNavBar = ({ links }) => {
             {/* ------- Pages Links -------- */}
             <div className='flex items-center gap-4'>
                 {links.map((link, index) => (
-                    <Link key={index} href={link.href} className={`${linkStyle} ${routerpath == link.href ? 'text-primary' : ''}`}>
+                    <Link key={index} href={link.href} className={`${linkStyle} ${routerpath == link.href ? 'text-primary' : 'text-custGray'}`}>
                         {link.label}
                     </Link>
                 ))}
